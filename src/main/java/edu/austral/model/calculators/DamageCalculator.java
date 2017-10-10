56596e6e481d1bf450a1c09c5ebf6de8516f71ef
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-10-10 19:32:26 -0300

    Added calculators

---
+++ src/main/java/edu/austral/model/calculators/DamageCalculator.java
@@ -0,0 +1,11 @@
+package edu.austral.model.calculators;
+
+import edu.austral.model.entities.Entity;
+
+public class DamageCalculator {
+
+    public static double calculateDamage(Entity collisioner, Entity collided){
+        // TODO implementar
+        return 0;
+    }
+}
---
+++ src/main/java/edu/austral/model/calculators/ScoreCalculator.java
@@ -0,0 +1,12 @@
+package edu.austral.model.calculators;
+
+import edu.austral.model.Player;
+import edu.austral.model.entities.Entity;
+
+public class ScoreCalculator {
+
+    public static int calculateScore(Player player, Entity kill){
+        // TODO implementar
+        return 0;
+    }
+}
